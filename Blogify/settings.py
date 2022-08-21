c40a35b2e5275d76d9b09b4fff292ac014d8a8e8
himanshu-rajawat <rajawathimanshu75@gmail.com>
2022-08-21 14:40:19 +0530

    another commit

--- Blogify/settings.py
+++ Blogify/settings.py
@@ -183,7 +183,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 # Then all the way at the bottom of the file
 # ....
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
